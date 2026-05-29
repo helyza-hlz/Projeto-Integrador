db52b35b6f94afa935a0892cc9bb0898f05cdc91
Helyza <helyzamorai2009@gmai.com>
2026-05-29 11:17:25 -0300

    AULA PI: Cores e Fontes

--- POO/carro.py
+++ POO/carro.py
@@ -1,26 +1,42 @@
-#A palavra "Class" é usada para criar uma classe.
-#Uma clase funciona como um molde para criar objetos 
-class Carro: 
+# A palavra "class" é usada para criar uma classe.
+# Uma classe funciona como um molde para criar objetos
+class Carro:
 
-    # "def" definir uma função ou método 
-# "__init__" é o método constutor da classe
+# "def" definir uma função ou método.
+# "__init__" é o método construtor da classe.
 # Ele é executado automaticamente quando um objeto é criado
-# "self" representa o próprio objto.
-# È através do self que acessamos atributos e métodos 
 
-# "marca", "modelo", "ano" e "velocidade" 
-#sao parametros recebidos pela classe 
+# "self" representa o próprio objeto.
+# É através do self que acessamos atributos e métodos do objeto
 
-    def __init__ (self, marca, modelo, ano, velocidade=0)
-    self.marca = marca
-    self.modelo = modelo
-    self.ano = ano
-    self.velocidade = velocidade
+# "marca", "modelo", "ano" e "velocidade"
+# São Parâmetros recebidos pela classe.
 
-# Criando um objeto de Classe Carro 
+    def __init__(self, marca, modelo, ano, velocidade=0):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+        self.velocidade = velocidade
+
+          # Métodos
+    # Método acelerar
+    def acelerar(self, aumento):
+        # self.velocidade = self.velocidade + aumento 
+        self.velocidade += aumento
+
+        print(f"O carro acelerou para{self.velocidade} km/h")
+
+# Criando um objeto da Classe Carro
 
 # "carro1" é uma variável que recebe um objeto
-carro1 = Carro ("Chevrolet", "S10", 2013)
+carro1 = Carro("Chevrolet", "S10", 2013)
 
-# Exibe informações do carro 
-print()
+# Exibir informações do carro
+print(f"Marca: {carro1.marca}")
+print(f"Modelo: {carro1.modelo}")
+print(f"Ano: {carro1.ano}")
+
+carro2 = Carro("Chevrolet", "Impala", 1967)
+print(f"Marca: {carro2.marca}")
+print(f"Modelo: {carro2.modelo}")
+print(f"Ano: {carro2.ano}")
